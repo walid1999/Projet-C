534327c8257c70e31906ba05f3873f0764bc9a53
Calvet2311 <compteblizzard23@gmail.com>
2020-03-04 10:51:40 +0100

    ajout projet

---
+++ jeu.beta/game.c
@@ -0,0 +1,50 @@
+void affichageMenu(){
+    int choixMenu;
+    do{
+        system("cls");
+        printf("\n\n\n\nMAIN MENU \n\n");
+        printf("1.Create New Game\n");
+        printf("2.Load Saved Game\n");
+        printf("3.About\n");
+        printf("4.Exit\n");
+        printf("\nVotre choix?\n\n");
+        scanf("%i", &choixMenu);
+
+        switch (choixMenu)
+        {
+        case 1:
+            {
+            printf("You selected Create New Game \n");
+            system("pause");
+            break;
+            (choixMenu == 4);
+            }
+            break;
+        case 2:
+            printf("You selected Load Save Game \n");
+            system("pause");
+            break;
+        case 3:
+            printf("Enzo HADEG \nRomain CALVET \nWalid BEN KHELFALLAH \n");
+            system("pause");
+            break;
+        case 4:
+            printf("\nBye!\n");
+            system("pause");
+            break;
+
+        default:
+            printf("\n er\n");
+            system("pause");
+            break;
+        }
+    }while   (choixMenu == 4);
+    {
+         char prenom[10];
+
+        printf("Entrez votre prenom : ");
+        scanf("%s",&prenom);
+        printf("\nBonjour %s", prenom);
+
+    }
+}
---
+++ jeu.beta/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    affichageMenu();
+    return 0;
+}
+#
